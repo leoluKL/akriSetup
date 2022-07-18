8ca50c4c2036fddd83ff48ecd120bdbbb6521cba
Leo Lu <leolu@microsoft.com>
2022-07-18 17:24:45 +0800

    dev

--- e2edemo/setupServer.sh
+++ e2edemo/setupServer.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
-main(){
+main() {
     eval $(parse_yaml initParams.yml)
 
     echo -e "\n\n----------------Setup virtual USB camera devices---------------\n"
-    
+    sudo apt update
+    sudo apt -y install linux-modules-extra-azure
+    sudo apt update
+    sudo apt -y install linux-headers-$(uname -r)
+    sudo apt -y install linux-modules-extra-$(uname -r)
+    sudo apt -y install dkms
+    curl http://deb.debian.org/debian/pool/main/v/v4l2loopback/v4l2loopback-dkms_0.12.5-1_all.deb -o v4l2loopback-dkms_0.12.5-1_all.deb
+    sudo dpkg -i v4l2loopback-dkms_0.12.5-1_all.deb
 }
 
-
 parse_yaml() {
-   local prefix=$2
-   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
-   sed -ne "s|^\($s\):|\1|" \
+    local prefix=$2
+    local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @ | tr @ '\034')
+    sed -ne "s|^\($s\):|\1|" \
         -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
-        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
-   awk -F$fs '{
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p" $1 |
+        awk -F$fs '{
       indent = length($1)/2;
       vname[indent] = $2;
       for (i in vname) {if (i > indent) {delete vname[i]}}
@@ -24,4 +30,5 @@ parse_yaml() {
    }'
 }
 
-main "$@"; exit
+main "$@"
+exit
