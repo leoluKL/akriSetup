4617af90f20a0a6d63a6601000cfcb2d971b0b86
Leo Lu <leolu@microsoft.com>
2022-07-20 13:16:01 +0800

    dev

---
+++ e2edemo/setupK3s_Helm_Akri.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+main() {
+    eval $(parse_yaml initParams.yml)
+
+    echo -e "\n\n----------------install K3s---------------\n"
+    curl -sfL https://get.k3s.io | sh -
+    echo -e "\n\n----------------grant admin privilege to access kube config---------------\n"
+    sudo addgroup k3s-admin
+    sudo adduser $USER k3s-admin
+    sudo usermod -a -G k3s-admin $USER
+    sudo chgrp k3s-admin /etc/rancher/k3s/k3s.yaml
+    sudo chmod g+r /etc/rancher/k3s/k3s.yaml
+    sudo su - $USER
+
+    echo -e "\n\n----------------install Helm---------------\n"
+    export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+    sudo apt install -y curl
+    curl -L https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+    export AKRI_HELM_CRICTL_CONFIGURATION="--set kubernetesDistro=k3s"
+
+    echo -e "\n\n----------------install Helm---------------\n"
+    helm repo add akri-helm-charts https://project-akri.github.io/akri/
+
+}
+
+parse_yaml() {
+    local prefix=$2
+    local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @ | tr @ '\034')
+    sed -ne "s|^\($s\):|\1|" \
+        -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p" $1 |
+        awk -F$fs '{
+      indent = length($1)/2;
+      vname[indent] = $2;
+      for (i in vname) {if (i > indent) {delete vname[i]}}
+      if (length($3) > 0) {
+         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+      }
+   }'
+}
+
+main "$@"
+exit
